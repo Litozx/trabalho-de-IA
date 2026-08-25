14e1470393f9dc592e041c7a317a68efb9d031b3
Alisson e Alexandre <alisson.antunes@escola.pr.gov.br>
2026-08-25 11:54:38 -0300

    c

--- main.js
+++ main.js
@@ -83,12 +83,16 @@ function mostraPergunta(){
   }
   perguntaAtual = perguntas[atual]
   caixaPerguntas.textContent = perguntaAtual.enunciado
+  caixaAlternativas.textContent = "";
   mostraAlternativas()
 }
 
 function mostraAlternativas(){
  for (const alternativas of perguntaAtual.alternativas){
     const botaoAlternativas=document.createElement("button")
+    botaoAlternativas.textContent = alternativa.texto
+    botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa))
+    
  }
 
 
